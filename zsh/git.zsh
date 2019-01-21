2dd4a373110ec683c7499219815b2c1061ad4ed0
Tobias Kunze <r@rixx.de>
2019-01-21 16:19:01 +0100

    Add useful git functions

---
+++ zsh/git.zsh
@@ -0,0 +1,47 @@
+
+alias g='git'
+alias ga='git add --verbose'
+alias gap='git add --patch'
+alias gc='git commit --verbose'
+alias gc!='git commit --verbose --amend'
+alias gca='git commit --verbose --all'
+alias gco='git checkout'
+alias gslog='git shortlog -ns'
+alias gd='git diff'
+alias gst='git status'
+alias git-pullall="find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;"
+
+
+function git-root () {
+	if [[ $(git rev-parse --is-inside-work-tree 2>&1) = "true" ]]
+	then
+		cd $(git rev-parse --show-toplevel)
+	fi
+}
+
+function gi() { curl -fL https://www.gitignore.io/api/${(j:,:)@} }
+
+# Run git status after some git commands
+# as inspired by Oleksandr Shybystyi oleksandr.shybystyi@gmail.com
+gitPreAutoStatusCommands=(
+    'add'
+    'rm'
+    'reset'
+    'commit'
+    'checkout'
+    'mv'
+)
+function elementInArray() {
+	local e
+	for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+	return 1
+}
+
+function gitStatusPrompt() {
+	command git "$@"
+
+	if (elementInArray $1 $gitPreAutoStatusCommands); then
+		command git status
+	fi
+}
+alias git=gitStatusPrompt
