c57515f96d47d837fc2b830bd4a5ec5ffecc0849
Tobias Kunze <r@rixx.de>
2021-09-01 13:10:45 +0200

    Add skeleton file for vue

---
+++ vim/templates/skeleton.vue
@@ -0,0 +1,27 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'viewOrComponentName',
+  components: { },
+  data () {
+    return { }
+  },
+  computed: {
+    something () {
+      return true
+    },
+  },
+  created () { },
+  mounted () { },
+  methods: {
+    somethingMethod (foo) {
+      return foo
+    },
+  }
+}
+</script>
+
+<style>
+</style>
