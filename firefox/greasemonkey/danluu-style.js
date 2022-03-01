2a2b10ac9f68329413b018a487e36d129b0fa41d
Tobias Kunze <r@rixx.de>
2022-03-01 05:55:37 +0100

    [firefox] Add styling for danluu's website

---
+++ firefox/greasemonkey/danluu-style.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name     danluu-style
+// @include  https://danluu.com/*
+// @version  1
+// ==/UserScript==
+
+document.querySelector("body").style = "max-width: 600px; margin: 0 auto; line-height: 1.8"
