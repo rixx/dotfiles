9a15b46b95c945e94a8fbcdc2782525773840977
Tobias Kunze <r@rixx.de>
2022-03-04 22:53:00 +0100

    [mail] Make systemd service run as user

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -2,7 +2,7 @@
 from subprocess import check_output
 
 def get_pass(account):
-    return check_output("PASSWORD_STORE_DIR=/home/rixx/.local/share/password-store pass Mail/" + account, shell=True).splitlines()[0]
+    return check_output("PASSWORD_STORE_DIR=/home/rixx/.local/share/password-store pass Mail/" + account, shell=True).splitlines()[0].decode()
 
 
 if __name__ == '__main__':
