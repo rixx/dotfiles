e08c4cb0c98cda337b49faebfe9296a87c401e55
Tobias Kunze <r@rixx.de>
2023-05-09 20:52:06 +0200

    [firefox] Improve ao3 script

--- firefox/greasemonkey/ao3-arrows.js
+++ firefox/greasemonkey/ao3-arrows.js
@@ -8,6 +8,10 @@
 // Add global keybinds for arrow-right and arrow-left
 
 document.addEventListener('keydown', (event) => {
+    // Only trigger if the user is not typing in a text box or text area or pressing a modifier key
+    if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA' || event.ctrlKey || event.altKey || event.metaKey || event.shiftKey) {
+        return
+    }
     if (event.key === 'ArrowRight') {
         document.querySelector('li.chapter.next a').click()
     }
