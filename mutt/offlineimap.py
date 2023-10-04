a2316c4a573ef1f801871d303018a6724efcd94f
Tobias Kunze <r@rixx.de>
2023-10-04 10:58:28 +0200

    [mutt] Switch password source

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -2,8 +2,8 @@
 from subprocess import check_output
 
 def get_pass(account):
-    return check_output("PASSWORD_STORE_DIR=/home/rixx/.local/share/password-store pass Mail/" + account, shell=True).splitlines()[0].decode()
+    return check_output("PASSWORD_STORE_DIR=/home/rixx/.local/share/password-store pass server/" + account, shell=True).splitlines()[0].decode()
 
 
 if __name__ == '__main__':
-    print(get_pass('cutebit'))
+    print(get_pass('tonks'))
