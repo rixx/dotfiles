edf985b9ec6646fee13dc0aaec85e41d20d66a60
Tobias Kunze <r@rixx.de>
2024-04-15 14:41:29 +0200

    [i3] Put djcrm in status bar

---
+++ i3/py3status/djcrm.py
@@ -0,0 +1,95 @@
+"""
+@author rixx
+"""
+
+import json
+import subprocess
+from functools import cached_property
+from time import time
+from datetime import datetime
+
+import requests
+
+
+class Py3status:
+    cache_timeout = 60
+    token = ""
+    url = ""
+    match_string = ""
+
+    @cached_property
+    def _base_url(self):
+        if self.url.startswith("https://"):
+            url = self.url[len("https://") :]
+        return url.rstrip("/")
+
+    @cached_property
+    def _api_url(self):
+        return f"https://{self._base_url}/api/invoices/?format=json&ordering=-nr"
+
+    def check_invoices(self, i3s_output_list, i3s_config):
+        response = {
+            "cached_until": time() + self.cache_timeout,
+            "full_text": "",
+            "color": i3s_config["color_good"],
+        }
+
+        try:
+            rsp = requests.get(
+                self._api_url,
+                headers={"Authorization": "Token {self.token}".format(self=self)},
+                verify=False,
+            )
+            invoices = rsp.json()["results"]
+        except Exception:
+            num = ""
+            response["color"] = i3s_config["color_bad"]
+            response["full_text"] = "€?"
+            return response
+
+        invoice_count = 0
+        money_sum = 0
+        current_month = datetime.now().strftime("%Y-%m")
+
+        for invoice in invoices:
+            if current_month not in invoice["date"]:
+                # Invoices are ordered by date, so we can stop here
+                break
+            num = max(0, json.loads(rsp.content.decode()).get("available_number", 0))
+
+            if self.match_string:
+                if not any(
+                    self.match_string in line["title"] for line in invoice["lines"]
+                ):
+                    continue
+
+            invoice_count += len(invoice["lines"])
+            money_sum += sum((float(line["total_net"]) for line in invoice["lines"]), 0)
+
+        money_sum = round(money_sum, 2)
+        response["full_text"] = f"€{money_sum:.2f} ({invoice_count})"
+        return response
+
+    def on_click(self, i3s_output_list, i3s_config, event):
+        subprocess.call(
+            [
+                "xdg-open",
+                f"https://{self._base_url}/admin/crmbase/invoice/stats/?costcenter=1",
+            ]
+        )
+
+
+if __name__ == "__main__":
+    """
+    Test this module by calling it directly.
+    """
+    from time import sleep
+
+    x = Py3status()
+    config = {
+        "color_good": "#00FF00",
+        "color_bad": "#FF0000",
+    }
+    while True:
+        print(x.check_invoices([], config))
+        sleep(1)
