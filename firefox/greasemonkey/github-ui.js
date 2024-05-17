1d941959816716073e88a8ae14f9f5c88d64f7b1
Tobias Kunze <r@rixx.de>
2024-05-17 15:41:26 +0200

    [firefox] Improve github UI script

--- firefox/greasemonkey/github-ui.js
+++ firefox/greasemonkey/github-ui.js
@@ -11,13 +11,13 @@
 
 const fixActionPage = () => {
     // check if we are on an actions page: /:org/:repo/actions/runs/:id
-    const path = window.location.pathname.split('/');
+    const path = window.location.pathname.split('/')
     if (path.length < 5 || path[4] !== 'runs') {
-        return;
+        return
     }
 
     // remove action graph
-    document.querySelector("action-graph").style.display = "none";
+    document.querySelector("action-graph").style.display = "none"
 
     /* reorder page: All entries of .PageLayout-region are reordered to:
      * 1. aria-label="Workflow run summary"
@@ -25,39 +25,65 @@ const fixActionPage = () => {
      * 3. aria-label="Annotations"
      * 4. everything else
      */
-    const regions = document.querySelectorAll('.PageLayout-region.PageLayout-content > div > *');
+    const regions = document.querySelectorAll('.PageLayout-region.PageLayout-content > div > *')
 
     const sortedRegions = Array.from(regions).sort((a, b) => {
         if (a.getAttribute('aria-label') === 'Workflow run summary') {
-            return -1;
+            return -1
         }
         if (b.getAttribute('aria-label') === 'Workflow run summary') {
-            return 1;
+            return 1
         }
         if (a.tagName === 'JOB-SUMMARIES') {
-            return -1;
+            return -1
         }
         if (b.tagName === 'JOB-SUMMARIES') {
-            return 1;
+            return 1
         }
         if (a.getAttribute('aria-label') === 'Annotations') {
-            return -1;
+            return -1
         }
         if (b.getAttribute('aria-label') === 'Annotations') {
-            return 1;
+            return 1
         }
-        return 0;
-    });
+        return 0
+    })
 
-    const parentElement = regions[0].parentNode;
+    const parentElement = regions[0].parentNode
     sortedRegions.forEach((region) => {
-        region.remove();
-        parentElement.appendChild(region);
-    });
+        region.remove()
+        parentElement.appendChild(region)
+    })
 }
 
+// Actions will be run on load and on page navigation
+const ACTIONS = [fixActionPage]
 
-document.addEventListener('DOMContentLoaded', fixActionPage);
+/*** SCAFFOLDING CODE ***/
+const observeUrlChange = () => {
+  // from https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
+  let oldHref = document.location.href;
+  const body = document.querySelector("body");
+  const observer = new MutationObserver(mutations => {
+    if (oldHref !== document.location.href) {
+      oldHref = document.location.href;
+      for (const action of ACTIONS) {
+        // we're running the action with a bit of delay to ensure the page is fully loaded
+        setTimeout(action, 500)
+      }
+    }
+  });
+  observer.observe(body, { childList: true, subtree: true });
+};
+
+window.onload = observeUrlChange;
+
+for (const action of ACTIONS) {
+  document.addEventListener('DOMContentLoaded', action)
+}
+
 if (document.readyState === 'complete') {
-  fixActionPage();
+  for (const action of ACTIONS) {
+    action()
+  }
 }
