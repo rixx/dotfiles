7bcc360e4b02dfc9409a7cf51d143587e3055550
Tobias Kunze <r@rixx.de>
2025-12-10 12:08:00 +0100

    [zsh] Try to keep homedir clean ffs

--- zsh/modules/tools.zsh
+++ zsh/modules/tools.zsh
@@ -61,12 +61,34 @@ alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 export TMUX_TMPDIR="$XDG_RUNTIME_DIR" 
 
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"
-#export PASSWORD_STORE_KEY="7A2D8B7875486954"
 
 export HTTPIE_CONFIG_DIR="$XDG_CONFIG_HOME/httpie"
 
 export ANSIBLE_LOCAL_TEMP=~/.cache/ansible/tmp
 export ANSIBLE_SSH_CONTROL_PATH_DIR=~/.cache/ansible/cp
 
-alias mycli="mycli --myclirc=~/.config/myclirc --logfile=/home/rixx/.cache/mycli.log"
-export MYCLI_HISTFILE=~/.local/share/mycli.hist
+export BUNDLE_USER_CACHE=$XDG_CACHE_HOME/bundle
+export BUNDLE_USER_CONFIG=$XDG_CONFIG_HOME/bundle/config
+export BUNDLE_USER_PLUGIN=$XDG_DATA_HOME/bundle
+
+export XAUTHORITY="$XDG_RUNTIME_DIR"/Xauthority
+
+export XCOMPOSEFILE="$XDG_CONFIG_HOME"/X11/XCompose
+export XCOMPOSECACHE="$XDG_CACHE_HOME"/X11/XCompose
+
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
+
+export PARALLEL_HOME="$XDG_CONFIG_HOME"/parallel
+
+export PYTHON_HISTORY=$XDG_STATE_HOME/python_history
+export PYTHONPYCACHEPREFIX=$XDG_CACHE_HOME/python
+export PYTHONUSERBASE=$XDG_DATA_HOME/python
+
+# Absolutely refuse to use xdg base dirs
+alias twine="twine --config-file $XDG_CONFIG_HOME/pypirc"
+
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+
+export KUBECONFIG="$XDG_CONFIG_HOME/kube"
+export KUBECACHEDIR="$XDG_CACHE_HOME/kube"
