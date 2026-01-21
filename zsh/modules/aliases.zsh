56f956d892330f71f6f8bbd67e0500e94c76b663
Tobias Kunze <r@rixx.de>
2026-01-21 08:27:49 +0100

    [zsh] Final cleanup before dropping it

--- zsh/modules/aliases.zsh
+++ zsh/modules/aliases.zsh
@@ -7,11 +7,6 @@ alias kssh="kitty +kitten ssh"
 compdef kssh=ssh
 
 if [ -x "$(command -v rsync)" ]; then
-    cpv() {
-	rsync -pogbr -hhh --backup-dir=/tmp/rsync -e /dev/null --progress "$@"
-    }
-    compdef _files cpv
-    alias cpr="rsync -ah --inplace --info=progress2"
     alias rsync-copy="rsync -avz --info=progress2 -h"
     alias rsync-move="rsync -avz --info=progress2 -h --remove-source-files"
     alias rsync-update="rsync -avzu --info=progress2 -h"
@@ -20,7 +15,6 @@ if [ -x "$(command -v rsync)" ]; then
     compdef rsync-move=rsync
     compdef rsync-update=rsync
     compdef rsync-sync=rsync
-    compdef cpr=rsync
 fi
 
 certcheck() {
@@ -69,13 +63,6 @@ compdef j=just
 alias pserver="uv run python -m http.server"
 alias ap='ansible-playbook --vault-password-file=.vault_password.sh'
 alias av='ansible-vault --vault-password-file=.vault_password.sh'
-
-function pyclean() {
-    ZSH_PYCLEAN_PLACES=${*:-'.'}
-    find ${ZSH_PYCLEAN_PLACES} -type f -name "*.py[co]" -delete
-    find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
-    find ${ZSH_PYCLEAN_PLACES} -type d -name ".mypy_cache" -delete
-}
 
 alias host='systemd-resolve'
 compdef host=systemd-resolve
--- zsh/modules/fzf.zsh
+++ zsh/modules/fzf.zsh
@@ -8,24 +8,7 @@ if type fzf > /dev/null; then
         export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
     fi
 
-    function fman() {
-        f=$(fd . /usr/share/man/man${1:-1} -t f -x echo {/.} | fzf --prompt="man > ") && man $f
-    }
-    function fkill() {  # In case kill<tab> was not provided
-        local pid
-        if [ "$UID" != "0" ]; then
-            pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-        else
-            pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-        fi
-        if [ "x$pid" != "x" ]
-        then
-            echo $pid | xargs kill -${1:-9}
-        fi
-    }
     FZF_DEFAULT_OPTS="--inline-info --cycle --border --color=light"
-
-    alias yaay="yay -Slq | fzf -m --preview 'yay -Si {1}'| xargs -ro yay -S"
 
     # Key bindings:
     # Alt-c opens a chdir
