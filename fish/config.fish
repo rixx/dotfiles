1cf1ada27b69e462ce1a22ca5e390e5402b64fa5
Tobias Kunze <r@rixx.de>
2026-01-21 09:08:59 +0100

    [fish] Fix startup when on server

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # Start sway if on VT 1-3
-if test -z "$DISPLAY"; and test "$XDG_VTNR" -le 3; and type -q sway
+if test -z "$DISPLAY"; and test -n "$XDG_VTNR"; and test "$XDG_VTNR" -le 3; and type -q sway
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
     set -gx SHELL /usr/bin/fish
     exec sway
