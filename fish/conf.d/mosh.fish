c8e769baf27fecd92e5764fcb96d0bc18e2fabfe
Tobias Kunze <r@rixx.de>
2026-01-22 09:59:46 +0100

    [fish] Improve mosh integration

--- fish/conf.d/mosh.fish
+++ fish/conf.d/mosh.fish
@@ -1,6 +1,7 @@
 # Source moshrc when running under mosh
 if test -f ~/.moshrc
-    set -l parent_cmd (ps -o comm= -p (status --ppid) 2>/dev/null | string trim)
+    set -l ppid (ps -o ppid= -p $fish_pid 2>/dev/null | string trim)
+    set -l parent_cmd (ps -o comm= -p $ppid 2>/dev/null | string trim)
     if test "$parent_cmd" = mosh-server
         source ~/.moshrc
     end
