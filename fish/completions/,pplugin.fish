8f12c5297f3bc682b7a88abd6d5e4d288887733f
Tobias Kunze <r@rixx.de>
2026-07-07 14:17:33 +0200

    [,pplugin] Add go command

--- fish/completions/,pplugin.fish
+++ fish/completions/,pplugin.fish
@@ -1,7 +1,15 @@
 # completions for ,pplugin
+
+# plugin names (with their path as description) for `go`, straight from the tool
+function __pplugin_names
+    command ',pplugin' -c list 2>/dev/null | string replace -r '\s\s+' \t
+end
+
 complete -c ',pplugin' -f
 complete -c ',pplugin' -n '__fish_use_subcommand' -s c -l cookiecutter -d 'include the cookiecutter template'
 complete -c ',pplugin' -n '__fish_use_subcommand' -a list -d 'print each maintained plugin and its location'
+complete -c ',pplugin' -n '__fish_use_subcommand' -a go -d 'cd to a named plugin'
 complete -c ',pplugin' -n '__fish_use_subcommand' -a all -d 'run a command in every plugin repo'
 complete -c ',pplugin' -n '__fish_use_subcommand' -a status -d 'git status for every plugin'
 complete -c ',pplugin' -n '__fish_use_subcommand' -a next -d 'cd to the next plugin repo'
+complete -c ',pplugin' -n '__fish_seen_subcommand_from go' -a '(__pplugin_names)'
--- fish/functions/,pplugin.fish
+++ fish/functions/,pplugin.fish
@@ -1,13 +1,13 @@
 function ',pplugin' --description 'manage owned pretalx plugins'
-    # `next` has to change *this* shell's directory, which the uv script (a
-    # subprocess) can't do, so we wrap it here. everything else is delegated
-    # straight to the script. `command` forces the binary, not this function.
+    # `next`/`go` have to change *this* shell's directory, which the uv script
+    # (a subprocess) can't do, so we wrap them here. everything else is
+    # delegated straight to the script. `command` forces the binary, not us.
     set -l rest $argv
     while test (count $rest) -ge 1; and contains -- $rest[1] -c --cookiecutter
         set rest $rest[2..-1]
     end
 
-    if test (count $rest) -ge 1; and test $rest[1] = next
+    if test (count $rest) -ge 1; and contains -- $rest[1] next go
         set -l dest (command ',pplugin' $argv)
         or return $status
         test -n "$dest"; and cd $dest
